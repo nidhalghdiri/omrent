677b85cccbe88e9cedfc2f3abb0b2325ff928ef4
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2024-11-06 13:23:02 +0400

    Listing creation (Image upload, Cloudinary CDN)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/duoeseunl/**", // Adjust this based on your specific Cloudinary path
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
