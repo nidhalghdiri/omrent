9e629b08725be6bd1fb15908338ee1b915b3caef
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2024-11-13 16:57:47 +0400

    Filters Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import RentModal from "./components/modals/RentModal";
 import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <LoginModal />
         <RegisterModal />
